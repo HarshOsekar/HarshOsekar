f81b0358add6283862a94ac3d8f4b89716ff7b95
Harsh Osekar <harshosekar@gmail.com>
2025-07-14 16:57:33 +0530

    added comments
    commented for understanding purpose

--- terraform-demo/variables.tf
+++ terraform-demo/variables.tf
@@ -1,12 +1,8 @@
+# AWS Provider & EC2
 variable "region" {
   description = "AWS region"
   type        = string
   default     = "us-east-1"
-}
-
-variable "bucket_name" {
-  description = "Name of the S3 bucket"
-  type        = string
 }
 
 variable "instance_type" {
@@ -16,12 +12,19 @@ variable "instance_type" {
 }
 
 variable "ami" {
-    description = "EC2 instance ami id"
-    type = string
-    default = "ami-0c02fb55956c7d316"
+  description = "EC2 instance ami id"
+  type        = string
+  default     = "ami-0c02fb55956c7d316"
 }
 
+# Tags
 variable "tags" {
   description = "Tags for AWS resources"
   type        = map(string)
+}
+
+# S3
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
 }
