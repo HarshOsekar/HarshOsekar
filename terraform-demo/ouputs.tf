cc858c085105646314cd50f0a883334dc1c77bfa
Harsh Osekar <harshosekar@gmail.com>
2025-07-14 17:02:23 +0530

    Update ouputs.tf
    changed from 2 modules to one modules which consist EC2 ID and Public IP info

--- terraform-demo/ouputs.tf
+++ terraform-demo/ouputs.tf
@@ -3,12 +3,10 @@ output "s3_bucket_name" {
   value       = aws_s3_bucket_acl.my_first_bucket.bucket
 }
 
-output "ec2_instance_id" {
+output "ec2_instance_info" {
   description = "The ID of the EC2 instance"
-  value       = aws_instance.my_ec2.id
-}
-
-output "ec2_public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.my_ec2.public_ip
+  value = {
+    instance_id = aws_instance.my_ec2.id
+    public_ip   = aws_instance.my_ec2.public_ip
+  }
 }
