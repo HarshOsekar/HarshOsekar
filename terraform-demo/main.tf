8dab73fd2bb0b6eac6c1dcc63a72ecb4e555f2a0
Harsh Osekar <harshosekar@gmail.com>
2025-07-14 14:55:25 +0530

    Create main.tf
    Added Terraform config with .gitignore to exclude state files

---
+++ terraform-demo/main.tf
@@ -0,0 +1,10 @@
+# Terraform file that creates an S3 bucket in AWS
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "my_first_bucket" {
+  bucket = "harsh-demo-s3-bucket-123456"
+  acl    = "private"
+}
