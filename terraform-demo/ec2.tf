2e9d7cc5c190cacff9c0c6923c93329ab0221c2f
Harsh Osekar <harshosekar@gmail.com>
2025-07-14 17:00:07 +0530

    Update ec2.tf
    tags mutilple values passed using map(string)

--- terraform-demo/ec2.tf
+++ terraform-demo/ec2.tf
@@ -3,9 +3,9 @@
 # }
 
 resource "aws_instance" "my_ec2" {
-  ami           = var.ami  # Amazon Linux 2 (free tier)
+  ami           = var.ami # Amazon Linux 2 (free tier)
   instance_type = var.instance_type
-  tags = var.tags
+  tags          = var.tags
 
   vpc_security_group_ids = [aws_security_group.harsh_ec2_sg.id]
 }
